92503d492445754145b235fc38b9009020e4899c
hml <807798553@qq.com>
2026-06-17 14:24:55 +0800

    【酒店列表】

--- lib/components/select_time.dart
+++ lib/components/select_time.dart
@@ -1,6 +1,7 @@
 import 'package:calendar_date_picker2/calendar_date_picker2.dart';
 import 'package:flutter/material.dart';
 import 'package:hotel_manage/util/select_time_range.dart';
+import 'package:hotel_manage/util/system_params.dart';
 
 class SelectTimeRang extends StatefulWidget {
   const SelectTimeRang({super.key});
@@ -29,6 +30,7 @@ class _SelectTimeState extends State<SelectTimeRang> {
   Widget build(BuildContext context) {
     return CalendarDatePicker2(
       config: CalendarDatePicker2Config(
+        dayBuilder: _dayBuild,
         calendarViewMode: CalendarDatePicker2Mode.scroll,
         calendarType: CalendarDatePicker2Type.range,
         weekdayLabelTextStyle: const TextStyle(
@@ -51,6 +53,47 @@ class _SelectTimeState extends State<SelectTimeRang> {
         }
         setState(() => _selectedDates = dates);
       },
+    );
+  }
+
+  Widget? _dayBuild({
+    required DateTime date,
+    TextStyle? textStyle,
+    BoxDecoration? decoration,
+    bool? isSelected,
+    bool? isDisabled,
+    bool? isToday,
+  }) {
+    if (isSelected == true) {
+      return _dayCommBuild(date, textStyle, primaryColor);
+    }
+    if (isToday == true) {
+      return _dayCommBuild(date, TextStyle(color: Colors.black54,fontSize: 12), Colors.transparent);
+    }
+    return _dayCommBuild(date, textStyle, Colors.transparent);
+  }
+  Widget _dayCommBuild(
+    DateTime date,
+    TextStyle? textStyle,
+    Color backgroundColor,
+  ) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: backgroundColor,
+      child: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: Text(
+                date.day.toString(),
+                style: textStyle,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
